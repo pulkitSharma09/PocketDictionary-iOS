859b7f43f327472e92ee624b34cf2733b23a67c1
Pulkit Sharma <pulkit.sharma0909@yahoo.in>
2015-12-12 13:14:27 +0530

    First Commit - Project created !

---
+++ PocketDictionary/PocketDictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PocketDictionary
+//
+//  Created by Pulkit on 12/12/15.
+//  Copyright © 2015 Pulkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PocketDictionary/PocketDictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PocketDictionary
+//
+//  Created by Pulkit on 12/12/15.
+//  Copyright © 2015 Pulkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
